eeb81643f7b6decc3d90313fb1958497168959d9
dmitriyTASHKINOV <tashkinov.mitiaaaa@gmail.com>
2024-07-27 15:26:13 +0300

    qwe

--- demo1/src/main/java/com/example/demo1/Controller/Controller.java
+++ demo1/src/main/java/com/example/demo1/Controller/Controller.java
@@ -2,11 +2,9 @@ package com.example.demo1.Controller;
 
 
 import com.example.demo1.Model.User;
-import com.example.demo1.Service.UserService;
-import com.example.demo1.repo.UserDao;
+import com.example.demo1.Service.UserService1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,12 +13,12 @@ import java.util.List;
 @org.springframework.stereotype.Controller
 public class Controller {
     @Autowired
-            private UserService userService;
+            private UserService1 userService1;
 
 
     @GetMapping(value = "/")
     public String printWelcome(Model model) {
-        List<User> userList = userService.getAllUsers();
+        List<User> userList = userService1.findAll();
         if(userList.isEmpty()) {
             model.addAttribute("emptyList", true);
         } else {
@@ -38,27 +36,27 @@ public class Controller {
 
     @PostMapping(value = "/addUser")
     public String addUserSubmit(User user, Model model) {
-        userService.addUser(user);
+        userService1.save(user);
         model.addAttribute("message", "Добавлен");
         return "redirect:/";
     }
 
     @GetMapping(value = "/editUser")
     public String editUserForm(@RequestParam Long id, Model model) {
-        User user = userService.getUserById(id);
+        User user = userService1.findById(id).orElse(null);
         model.addAttribute("user", user);
         return "editUser";
     }
 
     @PostMapping(value = "/editUser")
     public String editUserSubmit(User user) {
-        userService.updateUser(user);
+        userService1.save(user);
         return "redirect:/";
     }
 
     @PostMapping("/deleteUser")
     public String deleteUser(@RequestParam Long id, Model model) {
-        userService.deleteUser(id);
+        userService1.deleteById(id);
         model.addAttribute("message", "Пользователь успешно удален");
         return "redirect:/";
     }
---
+++ demo1/src/main/java/com/example/demo1/Service/UserService1.java
@@ -0,0 +1,6 @@
+package com.example.demo1.Service;
+
+import com.example.demo1.repo.UserDao;
+
+public interface UserService1 extends UserDao {
+}
